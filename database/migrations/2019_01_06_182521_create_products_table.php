bcf93f5bee45d35b13910cf75930cc7eef72c3e2
Rana Raheel Tariq <rana.raheel104@gmail.com>
2019-01-07 00:19:26 +0500

    init commit

---
+++ database/migrations/2019_01_06_182521_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->mediumText('description')->nullable();
+            $table->text('keywords')->unique();
+            $table->longText('details');
+            $table->enum('status',['verified','unverified']);
+            $table->boolean('availability')->default(1);
+            $table->bigInteger('price');
+            $table->string('image', 100);
+            $table->tinyInteger('brand_id');
+            $table->tinyInteger('category_id');
+            $table->integer('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
