40382c657bba08dedc2958f594ea5330530be947
Rana Raheel Tariq <rana.raheel104@gmail.com>
2019-01-25 20:36:44 +0500

    Intial Commit

---
+++ database/migrations/2019_01_24_190903_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('phone');
+            $table->date('dob');
+            $table->string('cnic');
+            $table->string('city', 100);
+            $table->string('institute')->nullable();
+            $table->unsignedinteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
